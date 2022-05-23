afdd38e6446b7767445389d6e8c15145ae76858f
zangdaoming <zangdaoming@vcinema.cn>
2022-05-23 22:43:44 +0800

    first

---
+++ say-hello/src/main/java/com/daoming/test/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.daoming.test;
+
+/**
+ * @Description TODO
+ * @Author daoming
+ * @Date 2022/5/23 10:41 PM
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
